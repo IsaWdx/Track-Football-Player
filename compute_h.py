a91243bfe3ffa29293a7ee539e01e1d52f4aa6b8
IsaWdx <isachriswang@163.com>
2015-10-30 22:29:26 +0800

    Done stitching
    The stitching process is a bit long however.

---
+++ compute_h.py
@@ -0,0 +1,76 @@
+import numpy as np
+import cv2
+__author__ = 'Wang Dongxu'
+
+
+def homography(pts, perspectivepts):
+    line0 = np.array([[pts[0][0],pts[0][1],1,0,0,0,-pts[0][0]*perspectivepts[0][0],-pts[0][1]*perspectivepts[0][0],-perspectivepts[0][0]]])
+    line1 = np.array([[0,0,0,pts[0][0],pts[0][1],1,-pts[0][0]*perspectivepts[0][1],-pts[0][1]*perspectivepts[0][1],-perspectivepts[0][1]]])
+
+    line2 = np.array([[pts[1][0],pts[1][1],1,0,0,0,-pts[1][0]*perspectivepts[1][0],-pts[1][1]*perspectivepts[1][0],-perspectivepts[1][0]]])
+    line3 = np.array([[0,0,0,pts[1][0],pts[1][1],1,-pts[1][0]*perspectivepts[1][1],-pts[1][1]*perspectivepts[1][1],-perspectivepts[1][1]]])
+
+    line4 = np.array([[pts[2][0],pts[2][1],1,0,0,0,-pts[2][0]*perspectivepts[2][0],-pts[2][1]*perspectivepts[2][0],-perspectivepts[2][0]]])
+    line5 = np.array([[0,0,0,pts[2][0],pts[2][1],1,-pts[2][0]*perspectivepts[2][1],-pts[2][1]*perspectivepts[2][1],-perspectivepts[2][1]]])
+
+    line6 = np.array([[pts[3][0],pts[3][1],1,0,0,0,-pts[3][0]*perspectivepts[3][0],-pts[3][1]*perspectivepts[3][0],-perspectivepts[3][0]]])
+    line7 = np.array([[0,0,0,pts[3][0],pts[3][1],1,-pts[3][0]*perspectivepts[3][1],-pts[3][1]*perspectivepts[3][1],-perspectivepts[3][1]]])
+
+    line8 = np.array([[pts[4][0],pts[4][1],1,0,0,0,-pts[4][0]*perspectivepts[4][0],-pts[4][1]*perspectivepts[4][0],-perspectivepts[4][0]]])
+    line9 = np.array([[0,0,0,pts[4][0],pts[4][1],1,-pts[4][0]*perspectivepts[4][1],-pts[4][1]*perspectivepts[4][1],-perspectivepts[4][1]]])
+
+    line = np.concatenate((line0,line1,line2,line3,line4,line5,line6,line7,line8,line9), axis=0)
+    U, s, V = np.linalg.svd(line, full_matrices=False)
+
+    min = s[0]
+    minnum = 0
+
+    for i in range(0, 9):
+        if(s[i]<min):
+            min = s[i]
+            minnum = i
+    solution = V[minnum,:]
+    h = np.zeros([3,3])
+    for i in range(0,9):
+        h[i/3][i%3] = solution[i]
+    return np.linalg.inv(h)
+
+
+
+left_img = cv2.imread("left_16.jpg")
+mid_img = cv2.imread("mid_16.jpg")
+
+
+
+left_pts = [[349,1630],
+            [328,1879],
+            [685,1799],
+            [569,1629],
+            [1003,1411]]
+
+mid_pts=[[295,118],
+         [243,345],
+         [576,351],
+         [505,168],
+         [991+1,47]]
+
+
+l2m = homography(left_pts, mid_pts)
+print l2m
+
+
+right_pts = [[574,312],
+             [1069,136],
+             [977,446],
+             [395,224+1],
+             [299,162+1]]
+
+mid_pts2 = [[600,1785],
+            [1051,1552],
+            [1050+1,1882],
+            [408,1717],
+            [309+1,1665+1]]
+r2m = homography(right_pts, mid_pts2)
+print r2m
+
+
---
+++ stitch.py
@@ -0,0 +1,83 @@
+import numpy as np
+import cv2
+import cv2.cv as cv
+__author__ = 'Wang Dongxu'
+
+l2m = [[ -1.69785456e+03,  -6.11337180e+01,  -7.05433527e+04],
+ [  2.50271374e+02,  -7.66438434e+02,  -2.68484830e+06],
+ [ -8.39661750e-02,   5.11169339e-01,  -1.69302582e+03]]
+r2m = [[  1.63972393e+03,   1.36522399e+02,  -2.49521497e+05],
+ [  2.00536899e+02,   1.74530149e+03,  -2.70504261e+06],
+ [  2.59606442e-02,   5.90282775e-01,   6.33491962e+02]]
+
+leftlength = 2900
+rightlength = 3700
+middlelength = 1920-3
+
+
+cap_l = cv2.VideoCapture("football_left.mp4")
+cap_m = cv2.VideoCapture("football_mid.mp4")
+cap_r = cv2.VideoCapture("football_right.mp4")
+
+fps = int(round(cap_l.get(cv.CV_CAP_PROP_FPS)))
+frame_size = [int(cap_l.get(cv.CV_CAP_PROP_FRAME_WIDTH )),int(cap_l.get(cv.CV_CAP_PROP_FRAME_HEIGHT))]
+
+fourcc = int(cap_l.get(cv.CV_CAP_PROP_FOURCC))
+iscolor = int(1)
+count = int(cap_l.get(7))
+
+newfoucc = int(cv2.cv.CV_FOURCC('M', 'P', 'E', 'G'))
+videoWriter = cv2.VideoWriter('out2.avi',
+                              newfoucc,
+                              fps, (frame_size[0]
+                              ,
+                               frame_size[1]/2))
+final_img  =  np.zeros([1080/2,1920,3])
+translated_location = np.zeros([1080/2,1920,2])
+
+
+for i in range(0,540):
+    for j in range(0,1920):
+        if j<leftlength*1920/8517+3:
+            a = np.dot(l2m,[i*2,(j-leftlength*1920/8517)*8517/1920,1])
+            scale = 1.0/a[2]
+            a[0]=int(scale*a[0])
+            a[1]=int(scale*a[1])
+
+            translated_location[i][j][0] = a[0]+4
+            translated_location[i][j][1] = a[1]
+        elif j<(leftlength+middlelength)*1920/8517:
+            translated_location[i][j][0] = i*2
+            translated_location[i][j][1] = j*8517/1920-leftlength
+        else:
+            a = np.dot(r2m,[i*2,j*8517/1920-leftlength,1])
+            scale = 1.0/a[2]
+            a[0]=int(scale*a[0])
+            a[1]=int(scale*a[1])
+            translated_location[i][j][0] = a[0]
+            translated_location[i][j][1] = a[1]
+
+
+
+for i in range(0,count):
+    _, left_img = cap_l.read()
+    _, mid_img = cap_m.read()
+    mid_img = mid_img[:,3:,:]
+    _,right_img = cap_r.read()
+    print i
+    for i in range(0,540):
+        for j in range(0,1920):
+            if j<leftlength*1920/8517+3:
+                if 0<=translated_location[i][j][0]<1080 and 0<=translated_location[i][j][1]<1920:
+                    final_img[i][j] = left_img[translated_location[i][j][0]][translated_location[i][j][1]]
+            elif j<(leftlength+middlelength)*1920/8517:
+                if 0<=translated_location[i][j][0]<1080 and 0<=translated_location[i][j][1]<1917:
+                    final_img[i][j] = mid_img[translated_location[i][j][0]][translated_location[i][j][1]]
+
+            else:
+                if 0<=translated_location[i][j][0]<1080 and 0<=translated_location[i][j][1]<1920:
+                    final_img[i][j] = right_img[translated_location[i][j][0]][translated_location[i][j][1]]
+
+
+    final_img = final_img .astype('uint8')
+    videoWriter.write(final_img)
